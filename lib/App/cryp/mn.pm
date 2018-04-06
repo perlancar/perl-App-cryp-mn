bd284fd1ef5be8a385967d9b0dab23b2a19c97d5
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-04-06 13:33:44 +0700

    - [Incompatible change] Rename subcommand ls to ls-masternodes
    - Add subcommand: ls-coins
    
    - ls-masternodes: Add coin filtering options --include-coin (-I), --exclude-coin
      (-X).

--- lib/App/cryp/mn.pm
+++ lib/App/cryp/mn.pm
@@ -8,6 +8,42 @@ use strict;
 use warnings;
 use Log::ger;
 
+our %arg_detail = (
+    detail => {
+        schema => 'bool*',
+        cmdline_aliases => {l=>{}},
+    },
+);
+
+our %args_filter_coins = (
+    include_coins => {
+        'x.name.is_plural' => 1,
+        'x.name.singular' => 'include_coin',
+        schema => ['array*', of=>'cryptocurrency::code*'],
+        cmdline_aliases => {I=>{}},
+        tags => ['category:filtering'],
+    },
+    exclude_coins => {
+        'x.name.is_plural' => 1,
+        'x.name.singular' => 'include_coin',
+        schema => ['array*', of=>'cryptocurrency::code*'],
+        cmdline_aliases => {X=>{}},
+        tags => ['category:filtering'],
+    },
+);
+
+sub _filter_coin {
+    my ($code, $args) = @_;
+
+    if ($args->{include_coins}) {
+        return 0 unless grep {$code eq $_} @{$args->{include_coins}};
+    }
+    if ($args->{exclude_coins}) {
+        return 0 if grep {$code eq $_} @{$args->{include_coins}};
+    }
+    1;
+}
+
 our %SPEC;
 
 $SPEC{':package'} = {
@@ -15,39 +51,73 @@ $SPEC{':package'} = {
     summary => 'Manage your masternodes',
 };
 
-$SPEC{list} = {
+$SPEC{list_coins} = {
+    v => 1.1,
+    summary => 'List supported coins',
+    args => {
+        %arg_detail,
+    },
+};
+sub list_coins {
+    require CryptoCurrency::Catalog;
+    require PERLANCAR::Module::List;
+
+    my %args = @_;
+
+    my $cat = CryptoCurrency::Catalog->new;
+
+    my $mods = PERLANCAR::Module::List::list_modules(
+        "App::cryp::Masternode::", {list_modules=>1});
+
+    my @res;
+    for my $mod (sort keys %$mods) {
+        my ($safename) = $mod =~ /::(\w+)\z/;
+        $safename =~ s/_/-/g;
+        my $coin = $cat->by_safename($safename);
+        push @res, {
+            code => $coin->{code},
+            safename => $coin->{safename},
+        };
+    }
+
+    unless ($args{detail}) {
+        @res = map { $_->{code} } @res;
+    }
+
+    my $resmeta = {};
+    $resmeta->{'table.fields'} = [qw/code safename/] if $args{detail};
+
+    [200, "OK", \@res, $resmeta];
+}
+
+$SPEC{list_masternodes} = {
     v => 1.1,
     summary => 'List your masternodes',
     args => {
-        detail => {
-            schema => 'bool*',
-            cmdline_aliases => {l=>{}},
-        },
+        %arg_detail,
+        %args_filter_coins,
         with_status => {
             schema => 'bool*',
             cmdline_aliases => {s=>{}},
         },
     },
 };
-sub list {
-    require CryptoCurrency::Catalog;
-    require PERLANCAR::Module::List;
-
+sub list_masternodes {
     my %args = @_;
 
     #use DDC; dd \%args;
 
-    my $mods = PERLANCAR::Module::List::list_modules(
-        "App::cryp::Masternode::", {list_modules=>1});
-
-    my $cat = CryptoCurrency::Catalog->new;
+    my $res = list_coins(detail=>1);
 
     my @res;
-    for my $mod (sort keys %$mods) {
-        my ($safename) = $mod =~ /::(\w+)\z/;
-        $safename =~ s/_/-/g;
-        log_trace "Listing masternodes for $safename ...";
-        my $coin = $cat->by_safename($safename);
+    for my $coin (@{$res->[2]}) {
+        log_trace "Listing masternodes for $coin->{code} ...";
+        unless (_filter_coin($coin->{code}, \%args)) {
+            log_trace "Skipping coin $coin->{code} (excluded)";
+            next;
+        }
+
+        (my $mod = "App::cryp::Masternode::$coin->{safename}") =~ s/-/_/g;
         (my $mod_pm = "$mod.pm") =~ s!::!/!g; require $mod_pm;
         my $drv = $mod->new;
         my $res = $drv->list_masternodes(
